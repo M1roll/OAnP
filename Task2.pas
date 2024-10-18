f17944a9f88495fcaa1c5341942bd7e1b39dd6df
M1roll <gludara104@gmail.com>
2024-10-18 22:46:45 +1000

    Update Task2.pas

---
+++ Task2.pas
@@ -0,0 +1,15 @@
+﻿program QeQ;
+var 
+  a, a2, a3, a5, ans:real;
+  f: text;
+
+begin
+  assign(f, 'in2.txt')
+  read(f, a);
+  Close(f);
+  a2 := a*a;
+  a3 := a2*a;
+  a5 := a3*a2;
+  ans := a5*a5*a3;
+  write(ans);
+end.
